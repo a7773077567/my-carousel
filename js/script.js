9d41f642b6d087cf9871e8c6d23ee8d806e8722b
AJay Li <andrewli707577@gmail.com>
2022-03-08 14:28:57 +0800

    feat: script finished

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
+let moving = false;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (!moving) {
+      disableInteraction();
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      console.log("8:offset ", offset);
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]");
+      console.log("12:slides ", slides);
+      const activeSlide = slides.querySelector("[data-active]");
+      console.log("14:activeSlide", activeSlide);
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+      console.log("16:newIndex ", newIndex);
+      if (newIndex < 0) newIndex = slides.children.length - 1;
+      if (newIndex > slides.children.length - 1) newIndex = 0;
+
+      slides.children[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
+    }
+  });
+});
+
+function disableInteraction() {
+  moving = true;
+  setTimeout(() => {
+    moving = false;
+  }, 500);
+}
